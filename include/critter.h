73b9d5ce7bfc0af59cb47476b571aedff78cba93
McNuggets <McNuggets@localhost>
2015-10-23 23:35:21 -0400

    made some directories and started in on critter.hpp (I think)

---
+++ include/critter.h
@@ -0,0 +1,35 @@
+#ifndef HW8_CRITTER_UTILS
+#define HW8_CRITTER_UTILS
+
+#include <string>
+
+class Critter{
+ public:
+  // returns the name of the Critter
+  std::string getName() const;
+
+  // changes the name of the Critter
+  void setName(std::stsring name);
+
+  // default constructor
+  Critter();
+
+  // destructor
+  ~Critter();
+
+
+ private:
+  // variables for describing the Critter
+  std::string name;
+  float cuteness;
+  float scariness;
+  float strangeness;
+
+  // featurs that this Critter has
+  Cover covering;
+  Eye eyes;
+  Limb limbs;
+  Digit digits;
+
+
+#endif
