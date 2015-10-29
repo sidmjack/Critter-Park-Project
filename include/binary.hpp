f5e0e783dce37d6d9fdada5d98e031353f25f12d
McNuggets <McNuggets@localhost>
2015-10-29 00:29:47 -0400

    Implemented but did not test cross(), mutate(), and printBinary().

--- include/binary.hpp
+++ include/binary.hpp
@@ -1,12 +1,46 @@
+/******************************************************************************
+ * Florian Pontani & Sidney Jackson
+ * fpontan1 & sjacks85
+ * (917) 454-8493 & (386) 956-5577
+ * 
+ * CS 600.120
+ * Homework #6, part 1
+ *
+ * binary.hpp - contains definitions for functions used in printing and
+ *     manipulating the binary strings used to store Critter traits.
+ *****************************************************************************/
+
+
 #ifndef HW8_BINARY_H
 #define HW8_BINARY_H
 
+//// if we need more than a long, change it here: ////
+///                                                ///
+             #define BINARY_TYPE long int          ///
+///                                                ///
+//////////////////////////////////////////////////////
+
+#include "binary.cpp"
+
 #include <vector>
 #include <map>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
 
+// utility for printing long ints in binary to the console
+void printBinary(const BINARY_TYPE number);
+
+// function for crossing two binary strings with a given jitter
+BINARY_TYPE cross(const BINARY_TYPE n1, const BINARY_TYPE n2, float jitter);
+
+// function for mutating a given binary string by a given factor
+BINARY_TYPE mutate(const BINARY_TYPE number, float severity);
+
+/*
 class Binary{
 	encode();
 	decode();
-}
-
+};
+*/
 #endif
---
+++ mutater.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "binary.hpp"
+
+int main(){
+  int a = 0;
+  int b = 405;
+
+  std::cout << "strings a and b are: \n";
+  printBinary(a);
+  std::cout << std::endl;
+  printBinary(b);
+  std::cout << std::endl << std::endl;
+
+  //test cross
+  printBinary(cross(a,b,0.0));
+  std::cout << std::endl;
+  printBinary(cross(b,a,0.0));
+  std::cout << std::endl;
+  printBinary(cross(a,b,1.0));
+  std::cout << std::endl;
+  printBinary(cross(a,b,0.5));
+  std::cout << std::endl;
+  //test mutate
+  mutate(b,0.5);
+  std::cout << std::endl;
+
+  return 0;
+}
